b0319a716b7e8aaf2d4f932f4f9291c4d792f70d
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-04-17 18:00:10 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
-  alias = "us-east-2"
+  region = "us-west-1"
+  alias = "us-west-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "us-west-2"
 }
